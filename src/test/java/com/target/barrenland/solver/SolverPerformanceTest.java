2c720c43c87b0a3322cc0b038ca9fa91ec898064
Nardin Vahidi Azar <nardin.azar@daugherty.com>
2020-03-20 02:21:07 -0500

    update test, update README

--- src/test/java/com/target/barrenland/solver/SolverPerformanceTest.java
+++ src/test/java/com/target/barrenland/solver/SolverPerformanceTest.java
@@ -2,6 +2,7 @@ package com.target.barrenland.solver;
 
 import com.target.barrenland.model.BarrenLand;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SolverPerformanceTest {
     @Test
@@ -13,6 +14,9 @@ public class SolverPerformanceTest {
             barrenLand.createBarrenLand(input);
             Solver solver = new Solver(barrenLand);
             solver.solve();
+            assertEquals(solver.getResults().size(), 2);
+            assertEquals(solver.getResults().get(0), Integer.valueOf(22816));
+            assertEquals(solver.getResults().get(1), Integer.valueOf(192608));
         }
         long endTime = System.currentTimeMillis();
 
@@ -20,22 +24,5 @@ public class SolverPerformanceTest {
                 " (including time to setup): " +  (endTime-startTime) + " ms.");
     }
 
-    @Test
-    public void testSolverPerformanceOnly() {
-        BarrenLand barrenLand = new BarrenLand(400, 600);
-        String input = "{\"48 192 351 207\", \"48 392 351 407\", \"120 52 135 547\", \"260 52 275 547\"}";
-        barrenLand.createBarrenLand(input);
-        long startTime = System.currentTimeMillis();
 
-        for(int i=0; i<100000; i++) {
-            Solver solver = new Solver(barrenLand);
-            solver.solve();
-        }
-        long endTime = System.currentTimeMillis();
-
-        System.out.println("Time to run 100000 iterations with array only solver" +
-                " (solve time only): " +  (endTime-startTime) + " ms.");
-
-
-    }
 }
